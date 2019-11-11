faa7dcbfed24a66878c70d281524516dee562832
db3design <51248858+db3design@users.noreply.github.com>
2019-11-11 14:05:10 -0500

    Reset

--- js/app.js
+++
@@ -1,7 +0,0 @@
-new fullpage('#fullpage', {
-    anchors:['firstPage', 'secondPage', 'thirdPage', 'fourthPage', 'fifthPage', 'sixthPage'],
-    sectionsColor: ['#4d05e8', '#e8ecf1', '#e8ecf1', '#e4e9ed'],
-    loopBottom: 'true',
-    fadingEffect: 'true',
-    slidesNavigation: true,
-});
