0fa0ee969e8f9ebe13c4d13d3de1c6385572af07
db3design <51248858+db3design@users.noreply.github.com>
2019-11-16 14:05:26 -0500

    added functionality and improved layout

--- js/heroButton.js
+++ js/heroButton.js
@@ -1,6 +1,55 @@
 $(function() {
     $('#scrollDown').click (function() {
-      $('html, body').animate({scrollTop: $('section#bio').offset().top }, '2500');
+      $('html, body').animate({scrollTop: $('section#bioIntro').offset().top }, '2500');
       return false;
     });
+  });
+
+
+  $(function() {
+    $('#bioLinkNav').click (function() {
+      $('html, body').animate({scrollTop: $('section#bioIntro').offset().top }, '2500');
+      return false;
+    });
+  });
+
+  $(function() {
+    $('#projectsLinkNav').click (function() {
+      $('html, body').animate({scrollTop: $('section#projects').offset().top }, '2500');
+      return false;
+    });
+  });
+
+  $(function() {
+    $('#conactLinkNav').click (function() {
+      $('html, body').animate({scrollTop: $('section#contact').offset().top }, '2500');
+      return false;
+    });
+  });
+
+  $(function() {
+    $('#brandIcon').click (function() {
+      $('html, body').animate({scrollTop: $('section#intro').offset().top }, '2500');
+      return false;
+    });
+  });
+
+  $(function() {
+    $('#heroProjectsButton').click (function() {
+      $('html, body').animate({scrollTop: $('section#projects').offset().top }, '2500');
+      return false;
+    });
+  });
+
+  $(function() {
+    $('#heroBioButton').click (function() {
+      $('html, body').animate({scrollTop: $('section#bioIntro').offset().top }, '2500');
+      return false;
+    });
+  });
+
+  var phoneButton = document.getElementById('giveMeACall');
+  
+  phoneButton.addEventListener('click', function () {
+    alert('Give Me A Call: +1-864-593-3007');
   });
--- js/scrollNav.js
+++ js/scrollNav.js
@@ -4,21 +4,28 @@ $(function() {
     var brand = $("div.brand > h3");
 
     var mobileNavIcon = $('#mobileNavIcon');
+
+    var navLinks = $('.navLinkContainer > .navLink');
   
     $(window).scroll(function() {    
         var scroll = $(window).scrollTop();
         if (scroll >= 50) {
             header.css({'background-color': '#F7F8F8', 'height': '85px', 'box-shadow': '0 3px 1.5px -3px #E1E0DF', 'border-color': '2px solid #F7F8F8'});
 
-            brand.css({'color': '#0C0C0C'});
+            brand.css({'color': '#0C0C0C', 'font-size': '2.25rem', });
 
             mobileNavIcon.css({'color': '#43C655'});
+
+            navLinks.css({'color': '#000000'});
+            
         } else {
             header.css({'background-color': 'transparent', 'height': '100px', 'box-shadow': 'none'});
 
-            brand.css({'color': 'white'});
+            brand.css({'color': 'white', 'font-size': '3rem'});
 
             mobileNavIcon.css({'color': '#E1E0DF'});
+
+            navLinks.css({'color': '#ffffff'});
         }
     });
   
